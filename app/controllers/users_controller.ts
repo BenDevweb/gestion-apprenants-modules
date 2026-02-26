d5fc922b2006192e0673c217390225735e6cb34f
BenDevweb <btechinnov746@hmail.com>
2026-02-26 18:03:14 +0100

    Integration des messages d'erreur  et de succes

--- app/controllers/users_controller.ts
+++ app/controllers/users_controller.ts
@@ -1,6 +1,6 @@
 import type { HttpContext } from '@adonisjs/core/http'
 import { loginValidator } from '#validators/login'
-import user from '#models/user'
+
 
 export type User = {
   id: number
@@ -19,7 +19,7 @@ export default class UsersController {
     return view.render('pages/register')
   }
 
-  async store({ request, response }: HttpContext) {
+  async store({ request, view }: HttpContext) {
     const payload = request.only(['name', 'email', 'password'])
     const user: User = {
       id: users.length + 1,
@@ -30,34 +30,34 @@ export default class UsersController {
 
     users.push(user)
 
-    return response.json({
-      message: 'Utilisateur enregistre en memoire',
-      user,
-      users,
-    })
+    return view.render('pages/login', {message: 'Compte créé avec succès. Veuillez vous connecter.'})
+    
   }
 
   async showLogin({ view }: HttpContext) {
     return view.render('pages/login')
   }
 
-  async login({ request, response, session }: HttpContext) {
-    try {
-      const payload = await loginValidator.validate(request.all())
+ async login({request, view}: HttpContext) {
+  try {
+    const payload = await request.validateUsing(loginValidator)
+    const user = users.find((u) => u.email === payload.email && u.password === payload.password)
 
-      const user = users.find(u => u.email === payload.email && u.password === payload.password)
-
-      if (!user) {
-        return response.status(401).json({ message: 'Invalid credentials' })
-      }
-
-      // Stocke l'ID de l'utilisateur dans la session
-      session.put('userId', user.id)
-
-      return response.json({ message: 'Login successful', user }) 
-    } catch (error: any) {
-      return response.status(400).json({ message: 'Validation failed', errors: error.errors })
-
+    if(!user) {
+      return view.render('pages/login', { errors: { message: 'Email ou mot de passe incorrect.' } })
     }
+    return view.render('pages/home', {message: `Bonjour et bienvenue ${user.email} ! Vous êtes connecté(e).`})
+  } catch (error: any) {
+    const errors = error?.messages ? error.messages : { message: 'Erreur lors de la connexion. Veuillez réessayer.'}
+    const fields: any = {}
+    if (error?.messages) {
+      error.messages.map(({ field, message }: any) => {
+        fields[field] = message
+      })
+      return view.render('pages/login', { errors: errors, fields: fields || {} })
+    }
+    return view.render('pages/login', { errors: errors })
   }
+ }
 } 
+
